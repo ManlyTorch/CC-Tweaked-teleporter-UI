7bc4b8906eeddc084d738a32881869cbc18dad81
ManlyTorch <34177141+ManlyTorch@users.noreply.github.com>
2025-08-12 09:57:00 -0700

    Update Boot.lua

--- Boot.lua
+++ Boot.lua
@@ -1,1 +1,13 @@
+function write(text, line)
+    local x, y = term.getSize()
+    term.setCursorPos(#text/2 + x, line)
+    write(text)
+end
 
+function redraw()
+    term.clear()
+    write("Teleports:", 1)
+    write("ABCDEFGHIJKLMNOP", 2)
+end
+
+redraw()
